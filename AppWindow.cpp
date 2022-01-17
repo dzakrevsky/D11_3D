2377bfe201f56a5d67ff47482e2af6a98b67a437
dzakrevsky <64309238+dzakrevsky@users.noreply.github.com>
2022-01-17 17:04:35 +0300

    game v0.11

--- AppWindow.cpp
+++ AppWindow.cpp
@@ -1,5 +1,16 @@
 #include "AppWindow.h"
 
+
+struct vec3
+{
+	float x, y, z;
+};
+
+struct vertex
+{
+	vec3 position;
+};
+
 AppWindow::AppWindow()
 {
 }
@@ -18,24 +29,39 @@ void AppWindow::onCreate()
 	RECT rc = this->getClientWindowRect();
 	m_swap_chain->init(this->m_hwnd, rc.right - rc.left, rc.bottom - rc.top);
 
+	vertex list[] =
+	{
+		{-0.5f,-0.5f,0.0f}, //pos1
+		{ 0.0f,0.5f,0.0f},  //pos2
+		{ 0.5f,-0.5f,0.0f}  //pos3
+	};
 
+	m_vb = GraphicsEngine::get()->createVertexBuffer();
+	UINT size_list = ARRAYSIZE(list);
 
-
+	GraphicsEngine::get()->createShaders();
+	void* shader_byte_code = nullptr;
+	UINT size_shader = 0;
+	GraphicsEngine::get()->getShaderBufferAndSize(&shader_byte_code, &size_shader);
+	m_vb->load(list, sizeof(vertex), size_list, shader_byte_code, size_shader);
 }
 
 void AppWindow::onUpdate()
 {
 	Window::onUpdate();
-	GraphicsEngine::get()->getImmediateDeviceContext()->clearRenderTargetColor(this->m_swap_chain, 1, 0, 0, 1);
-
-
-	m_swap_chain->present(false);
+	GraphicsEngine::get()->getImmediateDeviceContext()->clearRenderTargetColor(this->m_swap_chain, 0, 0.3f, 0.4f, 1);
+	RECT rc = this->getClientWindowRect();
+	GraphicsEngine::get()->getImmediateDeviceContext()->setViewportSize(rc.right - rc.left, rc.bottom - rc.top);
+	GraphicsEngine::get()->setShaders();
+	GraphicsEngine::get()->getImmediateDeviceContext()->setVertexBuffer(m_vb);
+	GraphicsEngine::get()->getImmediateDeviceContext()->drawTriangleList(m_vb->getSizeVertexList(), 0);
+	m_swap_chain->present(true);
 }
 
 void AppWindow::onDestroy()
 {
 	Window::onDestroy();
-
+	m_vb->release();
 	m_swap_chain->release();
 
 	GraphicsEngine::get()->release();
