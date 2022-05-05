65efb8e8ec9f6c77803da28edf1174fe08fbfcdd
dzakrevsky <64309238+dzakrevsky@users.noreply.github.com>
2022-05-05 04:45:46 +0300

    v0.4

--- AppWindow.h
+++ AppWindow.h
@@ -9,32 +9,55 @@
 #include "VertexShader.h"
 #include "PixelShader.h"
 #include "InputListener.h"
+#include "Matrix4x4.h"
 
 
 class AppWindow : public Window, public InputListener
 {
 public:
 	AppWindow();
-
-	void updateQuadPosition();
-
 	~AppWindow();
 
 	// Inherited via Window
 	virtual void onCreate() override;
 	virtual void onUpdate() override;
 	virtual void onDestroy() override;
+	virtual void onFocus() override;
+	virtual void onKillFocus() override;
+	virtual void onSize() override;
+
 
 	// Inherited via InputListener
 	virtual void onKeyDown(int key) override;
 	virtual void onKeyUp(int key) override;
+	virtual void onMouseMove(const Point& mouse_pos) override;
+
+	virtual void onLeftMouseDown(const Point& mouse_pos) override;
+	virtual void onLeftMouseUp(const Point& mouse_pos) override;
+	virtual void onRightMouseDown(const Point& mouse_pos) override;
+	virtual void onRightMouseUp(const Point& mouse_pos) override;
+public:
+	void render();
+	void update();
+	void updateModel();
+	void updateCamera();
+	void updateSkyBox();
+
+	void drawMesh(const MeshPtr& mesh, const VertexShaderPtr& vs, const PixelShaderPtr& ps, const ConstantBufferPtr& cb,
+		const TexturePtr& tex);
+
 private:
-	SwapChain* m_swap_chain;
-	VertexBuffer* m_vb;
-	VertexShader* m_vs;
-	PixelShader* m_ps;
-	ConstantBuffer* m_cb;
-	IndexBuffer* m_ib;
+	SwapChainPtr m_swap_chain;
+	VertexShaderPtr m_vs;
+	PixelShaderPtr m_ps;
+	PixelShaderPtr m_sky_ps;
+	ConstantBufferPtr m_cb;
+	ConstantBufferPtr m_sky_cb;
+	TexturePtr m_earth_color_tex;
+	TexturePtr m_earth_spec_tex;
+	TexturePtr m_sky_tex;	
+	MeshPtr m_mesh;
+	MeshPtr m_sky_mesh;
 private:
 	long m_old_delta;
 	long m_new_delta;
@@ -47,5 +70,20 @@ private:
 	float m_rot_x = 0.0f;
 	float m_rot_y = 0.0f;
 
+	float m_light_rot_y = 0.0f;
 
+
+	float m_scale_cube = 1;
+	float m_forward = 0.0f;
+	float m_rightward = 0.0f;
+
+	bool cull = true;
+	Matrix4x4 m_world_cam;
+	Matrix4x4 m_view_cam;
+	Matrix4x4 m_proj_cam;
+
+
+
+	bool m_play_state = false;
+	bool m_fullscreen_state = false;
 };
